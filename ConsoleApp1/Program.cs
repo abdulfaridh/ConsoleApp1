63534d841c53927e726c68e5b13267e03982dd6e
farid <farid@LAPTOP-SPTVSQVM>
2024-08-05 21:36:41 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi i am abdul");
+        }
+    }
+}
